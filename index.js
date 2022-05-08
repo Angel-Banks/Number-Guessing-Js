c0266e070a3496895dbd4fdd112bb392d2ca8672
Angel-Banks <angelonuchuks@gmail.com>
2022-05-08 16:10:18 +0100

    updated

---
+++ index.js
@@ -0,0 +1,25 @@
+var enterButton = document.getElementById("enterButton");
+var output = document.getElementById("output");
+var number = Math.floor(Math.random() * 100);
+
+
+enterButton.addEventListener('click', function() {
+  var input = document.getElementById("input").value;
+  if (input == number) {
+    output.innerHTML = "You guessed right, it was " + number;
+    output.style.color = "green";
+
+  } else if (input > number) {
+    output.innerHTML = "You guessed too high";
+
+  } else if (input < number) {
+    output.innerHTML = "You guessed too low";
+
+  } else if (isNaN(input)) {
+    output.innerHTML = "That's not a number!";
+
+  }
+});
+
+//  }  else if (isNaN (input)) {
+//    output.innerHTML= "That's not a number!";
